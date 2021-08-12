521d3aa84f05c9e0fc5f5aaf9b37892499e3fec8
TruongTuyen <hieudieuluong@gmail.com>
2021-08-12 13:51:32 +0700

    Add full width page template

--- templates/full-width-template.php
+++ templates/full-width-template.php
@@ -6,8 +6,11 @@
 
 get_header();
 
-?>
-	<p>This is an example template</p>
-<?php
-
+if ( have_posts() ) {
+	// Load posts loop.
+	while ( have_posts() ) {
+		the_post();
+		the_content();
+	}
+}
 get_footer();
