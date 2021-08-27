cadc9ea63583e4b72d7687797eacd3e714e03d9a
TruongTuyen <hieudieuluong@gmail.com>
2021-08-27 10:40:12 +0700

    Add single page content

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -1,21 +1,17 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
+<article id="post-<?php the_ID(); ?>" <?php post_class('prose-indigo prose-lg text-gray-500 mx-auto'); ?>>
+	<?php
+		if ( has_post_thumbnail() ) {
+			?>
+			<div class="post-thumbnail text-center my-10">
+				<?php the_post_thumbnail('full', array( 'class' => 'm-auto' ) ); ?>
+			</div>
+			<?php
+		}
+	?>
+	<h1 class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl p-10"><?php the_title(); ?></h1>
 	<div class="entry-content">
 		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Post title. Only visible to screen readers. */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'personalbridge' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			)
-		);
+		the_content();
 
 		wp_link_pages(
 			array(
@@ -26,8 +22,24 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<?php if ( ! is_singular( 'attachment' ) ) : ?>
-		<?php get_template_part( 'template-parts/post/author', 'bio' ); ?>
+	<?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Post title. Only visible to screen readers. */
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'personalbridge' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				),
+			);
+			?>
+		</footer><!-- .entry-footer -->
 	<?php endif; ?>
-
 </article><!-- #post-<?php the_ID(); ?> -->
